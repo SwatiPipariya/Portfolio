974d4c87a20bb62e2b0195b65d07f248669efff7
Swati <swati.pipariya6171@gmail.com>
2024-03-30 21:53:05 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import bg from "../../public/background/home-background.png"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+     <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25"/>
+
+      <div className="w-full h-screen ">
+          {/* Navigation and 3d model component */}
+          
+      </div>
+
+    </main>
+  );
+}
